046b22f71d19ff1d520691cabf16765b7e0aaaf8
rivero04 <ignaciorivero31@gmail.com>
2024-10-31 18:19:13 -0300

    Actualizar dependencias y reestructurar la página de inicio con formulario de inicio de sesión(SOLO FRONTEND, la logica todavia no)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,72 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import Image from 'next/image';
+import logo from './logo.svg';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [showPassword, setShowPassword] = useState(false);
+
+  const PasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <section className="bg-[#F5F7FA]">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen">
+        <Image src={logo} className="w-40 h-30 mr-2 mb-3" alt="logo" />
+        <div className="w-full bg-white rounded-lg  md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl text-center font-bold text-gray-900 md:text-2xl">
+              Inicia Sesión
+            </h1>
+            <form className="space-y-4 md:space-y-6">
+                <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-bold text-gray-700">E-mail</label>
+                <input
+                  type="email"
+                  id="email"
+                  className="bg-white border text-gray-900 rounded-lg block w-full p-2.5 focus:outline-none"
+                  placeholder="Introduce tu email"
+                  required
+                />
+                </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-bold text-gray-700">Contraseña</label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    placeholder="Introduce tu contraseña"
+                    className="bg-white border  text-gray-900 rounded-lg block w-full p-2.5 pr-10"
+                    required
+                  />
+                    <button
+                    type="button"
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-600"
+                    onClick={PasswordVisibility}
+                    >
+                    {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+                    </button>
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <button
+                  type="submit"
+                  className="w-full text-white bg-[#F72793] hover:bg-pink-600 font-bold rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Ingresar
+                </button>
+                <div className="flex justify-end w-full">
+                  <a href="#" className="mt-2 text-sm font-medium text-gray-900">¿Olvidaste tu contraseña?</a>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </section>
   );
 }
